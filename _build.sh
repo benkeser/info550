7bb26792399e97a06adc8131aa25d534751d1707
David Benkeser <benkeser@emory.edu>
2020-08-25 20:41:04 +0000

    update the website.

--- _build.sh
+++ _build.sh
@@ -18,15 +18,22 @@ RMD_LECTURE_FILES=$(find "lectures" -type f -name "*.Rmd")
 # finds all .Rmd homework files
 RMD_HOMEWORK_FILES=$(find "homework" -type f -name "*.Rmd")
 
-# build Rmd files using Rmarkdown
+# build Rmd lecture files
 for RMD_FILE in ${RMD_LECTURE_FILES}
 do 
 	FILE_PATH=${RMD_FILE%/*}
 	FILE_NAME=${RMD_FILE##*/}
+	# build rmarkdown slides
 	Rscript -e "setwd('${TRAVIS_BUILD_DIR}/${FILE_PATH}/'); rmarkdown::render('${FILE_NAME}')"
+	# grab bash code from slides if it exists
+	BASH_FILE_NAME="$(echo "${FILE_NAME}" | cut -f 1 -d '.').bash"
+	cd ${TRAVIS_BUILD_DIR}/${FILE_PATH}/
+	if [ -f "./.grab-bash" ]; then
+        awk -F ', *' '/^```$/{p = 0} p; /```{bash/ && !/ (include)/{print "\n###", $2, "###"; p=1}' ${FILE_NAME} > $BASH_FILE_NAME
+	fi	
 done
 
-# build Rmd files using Rmarkdown
+# build Rmd homework files
 for RMD_FILE in ${RMD_HOMEWORK_FILES}
 do 
 	FILE_PATH=${RMD_FILE%/*}
