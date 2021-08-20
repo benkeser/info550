cc12ec4c22cbccb5baeb48dfa4232aa8f0351abf
Benkeser <benkeser@emory.edu>
2021-08-20 15:00:37 -0400

    add new script

--- _build_site.sh
+++ _build_site.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 # switch to a new temporary branch
+cd ..
 mkdir build_dir
 cd build_dir
 export TRAVIS_BUILD_DIR=$(pwd)
@@ -11,13 +12,11 @@ git clone -b gh-pages \
   https://github.com/benkeser/info550 \
   info550
 
-git checkout -b temporary
-
 # run website/_build.sh 
 website/_build.sh
 
 # run website/_deploy.sh local
-website/_deploy.sh
+website/_deploy.sh local
 
 # rm info550/info550 dir
 cd TRAVIS_BUILD_DIR/..
