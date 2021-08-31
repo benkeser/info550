4af7bf7b876edaf3ba1235327c9e238ca3ed4b84
Benkeser <benkeser@emory.edu>
2021-08-31 15:20:09 -0400

    update the website.

--- _build.sh
+++ _build.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
+Rscript -e "renv::restore(prompt = FALSE)"
 #--------------------------------------------------------
 # build pdf of syllabus
 #--------------------------------------------------------
 # remove the bit about downloading pdf
 
 sed '/Download a pdf copy of syllabus/d' ${TRAVIS_BUILD_DIR}/website/syllabus.md > ${TRAVIS_BUILD_DIR}/website/syllabus_mod.md
-Rscript -e "setwd('${TRAVIS_BUILD_DIR}/website/'); rmarkdown::render('syllabus_mod.md', output_file = 'syllabus.pdf')"
+Rscript -e "renv::activate(); setwd('${TRAVIS_BUILD_DIR}/website/'); rmarkdown::render('syllabus_mod.md', output_file = 'syllabus.pdf')"
 rm ${TRAVIS_BUILD_DIR}/website/syllabus_mod.md
 
 #--------------------------------------------------------
@@ -24,7 +25,7 @@ do
 	FILE_PATH=${RMD_FILE%/*}
 	FILE_NAME=${RMD_FILE##*/}
 	# build rmarkdown slides
-	Rscript -e "setwd('${TRAVIS_BUILD_DIR}/${FILE_PATH}/'); rmarkdown::render('${FILE_NAME}')"
+	Rscript -e "renv::activate(); setwd('${TRAVIS_BUILD_DIR}/${FILE_PATH}/'); rmarkdown::render('${FILE_NAME}')"
 	# grab bash code from slides if it exists
 	BASH_FILE_NAME="$(echo "${FILE_NAME}" | cut -f 1 -d '.').sh"
 	if [ -f "${TRAVIS_BUILD_DIR}/${FILE_PATH}/.grab-bash" ]; then
@@ -37,7 +38,7 @@ for RMD_FILE in ${RMD_HOMEWORK_FILES}
 do 
 	FILE_PATH=${RMD_FILE%/*}
 	FILE_NAME=${RMD_FILE##*/}
-	Rscript -e "setwd('${TRAVIS_BUILD_DIR}/${FILE_PATH}/'); rmarkdown::render('${FILE_NAME}')"
+	Rscript -e "renv::activate(); setwd('${TRAVIS_BUILD_DIR}/${FILE_PATH}/'); rmarkdown::render('${FILE_NAME}')"
 done
 
 # make a directory for recording information
