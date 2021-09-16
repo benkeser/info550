9e3b5cfe6825ffba0b7223ac9ed02248c632a023
Benkeser <benkeser@emory.edu>
2021-09-16 15:57:18 -0400

    add recording of installing wsl in sublime

--- website/make_lecture_data.py
+++ website/make_lecture_data.py
@@ -135,7 +135,8 @@ make_data(input_dir, "homework")
 video_links = {
   "vim-survival-guide" : "16e493ae-2574-4eb9-b48e-ac2501201b59",
   "installing-wsl" : "b727d059-1996-4456-be5f-ad940005ea62",
-  "installing-r-wsl" : "67d7ed3a-ca99-441e-8bd8-ac3300efb3cd"
+  "installing-r-wsl" : "67d7ed3a-ca99-441e-8bd8-ac3300efb3cd",
+  "wsl-in-sublime" : "4b6f4b7e-bb84-41ec-b04e-ada50141f670"
 }
 
 for key in video_links:
